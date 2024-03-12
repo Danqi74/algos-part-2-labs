10011b0c9eab305b701f05336efce06bfb8ae2f9
Danqi74 <dan.hnyp@gmail.com>
2024-03-12 08:43:47 +0200

    Optimize memory usage

--- scr/tree_diameter.py
+++ scr/tree_diameter.py
@@ -11,9 +11,7 @@ def _binary_tree_diameter(tree: BinaryTree) -> int:
     left_diameter, left_tree_length = _binary_tree_diameter(tree.left)
     right_diameter, right_tree_length = _binary_tree_diameter(tree.right)
     current_node_diameter = left_tree_length + right_tree_length
-    length_left = left_tree_length + 1
-    length_right = right_tree_length + 1
-    return max(left_diameter, right_diameter, current_node_diameter), max(length_left, length_right)
+    return max(left_diameter, right_diameter, current_node_diameter), max(left_tree_length, right_tree_length) + 1
 
 
 def binary_tree_diameter(tree: BinaryTree) -> int:
