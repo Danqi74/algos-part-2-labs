6da8f6607421039fd1b47a0bd319df620339ed4c
Danqi74 <dan.hnyp@gmail.com>
2024-04-11 22:13:27 +0300

    add lab4 files

---
+++ test/test_binary_tree_priority_queue.py
@@ -0,0 +1,69 @@
+import unittest
+
+from importmonkey import add_path
+
+add_path("../")
+
+from src.binary_tree_priority_queue import PriorityQueue
+
+
+class TestPriorityQueue(unittest.TestCase):
+    def test_insert(self):
+        queue = PriorityQueue()
+        queue.insert("SomeTask 1", 5)
+        queue.insert("SomeTask 2", 8)
+        queue.insert("SomeTask 3", 3)
+        queue.insert("SomeTask 4", 10)
+        queue.insert("SomeTask 5", 9)
+        queue.insert("SomeTask 6", 4)
+        queue.insert("SomeTask 7", 1)
+        expected_queue = [
+            ("SomeTask 4", 10),
+            ("SomeTask 5", 9),
+            ("SomeTask 2", 8),
+            ("SomeTask 1", 5),
+            ("SomeTask 6", 4),
+            ("SomeTask 3", 3),
+            ("SomeTask 7", 1),
+        ]
+        self.assertEqual(queue.inorder_traversal(), expected_queue)
+
+    def test_pop(self):
+        queue = PriorityQueue()
+        queue.insert("SomeTask 1", 5)
+        queue.insert("SomeTask 2", 8)
+        queue.insert("SomeTask 3", 3)
+        queue.insert("SomeTask 4", 10)
+        queue.insert("SomeTask 5", 9)
+        queue.insert("SomeTask 6", 4)
+        queue.insert("SomeTask 7", 1)
+
+        popped_task = queue.pop()
+        self.assertEqual(popped_task, "SomeTask 4")
+        popped_task = queue.pop()
+        self.assertEqual(popped_task, "SomeTask 5")
+
+        expected_queue = [
+            ("SomeTask 2", 8),
+            ("SomeTask 1", 5),
+            ("SomeTask 6", 4),
+            ("SomeTask 3", 3),
+            ("SomeTask 7", 1),
+        ]
+        self.assertEqual(queue.inorder_traversal(), expected_queue)
+
+    def test_pop_empty_queue(self):
+        queue = PriorityQueue()
+        popped_task = queue.pop()
+        self.assertIsNone(popped_task)
+
+    def test_pop_single_task(self):
+        queue = PriorityQueue()
+        queue.insert("SomeTask 1", 5)
+        popped_task = queue.pop()
+        self.assertEqual(popped_task, "SomeTask 1")
+        self.assertEqual(queue.inorder_traversal(), None)
+
+
+if __name__ == "__main__":
+    unittest.main()
