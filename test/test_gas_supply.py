34c6d7129954ad60ee989498d6e028c92d0b9026
Danqi74 <dan.hnyp@gmail.com>
2024-04-19 00:29:34 +0300

    Add lab6 files (gas supply for penguins)

---
+++ test/test_gas_supply.py
@@ -0,0 +1,52 @@
+import unittest
+
+from importmonkey import add_path
+
+add_path("../src/")
+
+from gas_supply import check_gas_supply
+
+
+class TestGasSupply(unittest.TestCase):
+    def test_without_connections(self):
+        cities = ["a", "b", "c"]
+        gas_storages = ["1", "2"]
+        pipelines = []
+        expected_result = [["1", ["a", "b", "c"]], ["2", ["a", "b", "c"]]]
+        result = check_gas_supply(cities, gas_storages, pipelines)
+        self.assertEqual(result, expected_result)
+
+    def test_with_all_connections(self):
+        cities = ["a", "b", "c"]
+        gas_storages = ["1", "2"]
+        pipelines = [
+            ["1", "a"],
+            ["1", "b"],
+            ["1", "c"],
+            ["2", "a"],
+            ["2", "b"],
+            ["2", "c"],
+        ]
+        expected_result = []
+        result = check_gas_supply(cities, gas_storages, pipelines)
+        self.assertEqual(result, expected_result)
+
+    def test_with_transit(self):
+        cities = ["a", "b", "c"]
+        gas_storages = ["1", "2"]
+        pipelines = [["1", "a"], ["a", "b"], ["b", "c"], ["2", "1"]]
+        expected_result = []
+        result = check_gas_supply(cities, gas_storages, pipelines)
+        self.assertEqual(result, expected_result)
+
+    def test_with_connections_between_cities(self):
+        cities = ["a", "b", "c"]
+        gas_storages = ["1", "2"]
+        pipelines = [["1", "a"], ["a", "b"], ["2", "c"], ["1", "2"]]
+        expected_result = [["2", ["a", "b"]]]
+        result = check_gas_supply(cities, gas_storages, pipelines)
+        self.assertEqual(result, expected_result)
+
+
+if __name__ == "__main__":
+    unittest.main()
